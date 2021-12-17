dd0444e4998d042fafa60c147d95de6330861126
DeepGamit <deepkgamit@gmail.com>
2021-12-17 15:39:47 -0500

    Final Project

---
+++ EKS/c-nodes-eks-worker-nodes-asg.tf
@@ -0,0 +1,29 @@
+# usage of the specific kubernetes.io/cluster/* resource tag is required for 
+# EKS and Kubernetes to discover and manage compute resources
+
+# Note:
+# vpc_zone_identifier  = ["${aws_subnet.demo.*.id}"]
+# was replaced with:
+# vpc_zone_identifier  = "${aws_subnet.demo.*.id}"
+
+resource "aws_autoscaling_group" "final" {
+  desired_capacity     = 2
+  launch_configuration = "${aws_launch_configuration.final.id}"
+  max_size             = 2
+  min_size             = 1
+  name                 = "terraform-eks-final"
+  #vpc_zone_identifier  = "${aws_subnet.demo.*.id}"
+  vpc_zone_identifier  = aws_subnet.final.*.id
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-eks-final"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "kubernetes.io/cluster/${var.cluster-name}"
+    value               = "owned"
+    propagate_at_launch = true
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CSYE7220FinalProject
+# CSYE 7220 - Devolpment Operations
+
+## Team Members
+- Deep Gamit (001057437)
+- Priyam Suthar (001054934)
+
+## For the instructions about how to run please refer Manifest File.
